8db5342aba7a9b97d3305e4620f0bf447435d00e
Lotta Kapulainen <lotta20000@student.hamk.fi>
2021-09-16 13:32:27 +0300

    uusi

--- testi12333/Program.cs
+++ testi12333/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 
-namespace testi12333
+
+Console.WriteLine("Syötä numero: ");
+Console.WriteLine("1. Näytä työntekijät");
+Console.WriteLine("2. Lisää työntekijä");
+Console.WriteLine("3. Näytä palkkamaksupäivät");
+
+var vastaus1 = Console.ReadLine();
+
+if (vastaus1 == "1")
 {
-    class Program
+    Console.WriteLine("Valitse työntekijä: ");
+
+    Console.WriteLine("Lista työntekijöistä"); 
+    //lista users
+
+    var vastaus2 = Console.ReadLine();
+
+    if (vastaus2 == "1")
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        Console.WriteLine("Matti Meikäläinen, palkka, palkanrakenne, palkkapäivä");
+
+        // nollasta alkuvalikkoon
     }
+}
+if (vastaus1 == "2")
+{
+    Console.WriteLine("Syötä työntekijän tiedot: ");
+    // tähän tulee listaan lisääminen
+    // näytä lista
+    Console.WriteLine("Työntekijä lisätty listaan!");
+}
+
+if (vastaus1 =="3")
+{
+    //näytä työntekijöitten palkkapäivät
+}
+
+else
+{
+    //error
 }
