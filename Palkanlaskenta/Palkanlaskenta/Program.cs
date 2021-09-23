5602c4f60849dc8979c68b80b67860bf4986d422
joonasaaltonen918 <89994387+joonasaaltonen918@users.noreply.github.com>
2021-09-23 13:08:06 +0300

    Add files via upload

---
+++ Palkanlaskenta/Palkanlaskenta/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Palkanlaskenta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
