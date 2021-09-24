da32ec4cecb6ec3892883478a1018d808810cb94
lottakapulainen <80256323+lottakapulainen@users.noreply.github.com>
2021-09-24 11:12:08 +0300

    Add files via upload

---
+++ Palkanlaskenta/SovellusEsim1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace SovellusEsim1
+{
+    class Program
+    {
+        static string ohje = "0.Lopetus\n1.Lisää\n2.Listaa";
+
+        static void Main(string[] args)
+        {
+            Sovellus app = new Sovellus();
+
+            Console.WriteLine(ohje);
+            string komento = Console.ReadLine();
+            while (!komento.StartsWith("0"))   // ! tarkoittaa NOT
+            {
+                switch (komento)
+                {
+                    case "1":
+                        Console.WriteLine("Anna työntekijän nimi");
+                        var nimi = Console.ReadLine();
+
+                        Console.WriteLine("Anna palkkatiedot");
+                        var palkkatiedot = Console.ReadLine();
+
+                        Console.WriteLine("Anna ennakonpidätysprosentti");
+                        var Ennakonpidätys = Console.ReadLine();
+
+                        Console.WriteLine("Anna palkanmaksupäivä");
+                        var Palkanmaksupäivä = Console.ReadLine();
+
+
+
+                        var työntekijä = new Työntekijä(palkkatiedot,nimi,Ennakonpidätys,Palkanmaksupäivä);                        
+
+                        app.LisääTyöntekijä(työntekijä);
+                        break;
+
+                    case "2":
+                        Console.WriteLine(app.ListaaTyöntekijät());
+                        break;
+
+                    default:        // kaikissa muissa tapauksissa
+                        break;
+                }
+                Console.WriteLine(ohje);
+                komento = Console.ReadLine();
+            }
+        }
+    }
+}
